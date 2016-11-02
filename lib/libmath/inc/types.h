4a045764197bf0140cfc967774b91c00dd7f7cea
Remi Laot <remilaot@gmail.com>
2016-11-02 20:04:16 +0100

    initial commit - matrices

---
+++ lib/libmath/inc/types.h
@@ -0,0 +1,11 @@
+namespace math
+{
+  struct mat<col_size, row_size, Type>
+  {
+    
+  };
+  
+  using vec2_f = mat<1,2,float>;
+  using vec3_f = mat<1,2,float>;
+  using quat<Type> = mat<1,2,Type>;
+}
---
+++ lib/libmath/readme.md
@@ -0,0 +1,5 @@
+# vectors, quaternions, matrices ...
+
+OpenGL uses column major matrices, thus so do we.
+
+vectors and quaternions are represented as row matrices;
