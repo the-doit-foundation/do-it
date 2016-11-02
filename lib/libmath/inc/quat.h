592b0ed4be56fb960b9ac16a35a2c87336128a9c
Remi Laot <remilaot@gmail.com>
2016-11-02 21:15:17 +0100

    vectors start

--- lib/libmath/inc/quat.h
+++ lib/libmath/inc/quat.h
@@ -8,5 +8,20 @@ namespace math
       Type w, x, y, z;
       Type data[4];
     }
+    
+    inline Type & operator[](size_t i);
+    inline quat<Type> & operator-() const;
+
+    inline quat<Type> & operator*(const Type scalar) const;
+    inline quat<Type> & operator/(const Type scalar) const;
+
+    inline friend quat<Type> & operator*(const Type scalar, const quat<Type> vec) const;
+    inline friend quat<Type> & operator/(const Type scalar, const quat<Type> vec) const;
+
+    inline quat<Type> & operator+(const quat<Type> & other) const;
+    inline quat<Type> & operator-(const quat<Type> & other) const;
+
+    inline quat<Type> & operator*(const quat<Type> & other) const;
+    inline friend quat<Type> & operator*(const quat<Type> & a, const quat<Type> & b);
   };
 };
--- lib/libmath/inc/vec3.h
+++ lib/libmath/inc/vec3.h
@@ -8,5 +8,24 @@ namespace vec3
       Type x, y, z;
       Type data[3];
     }
+
+    inline Type & operator[](size_t i);
+    inline vec3<Type> & operator-() const;
+
+    inline vec3<Type> & operator*(const Type scalar) const;
+    inline vec3<Type> & operator/(const Type scalar) const;
+
+    inline friend vec3<Type> & operator*(const Type scalar, const vec3<Type> vec) const;
+    inline friend vec3<Type> & operator/(const Type scalar, const vec3<Type> vec) const;
+
+    inline vec3<Type> & operator+(const vec3<Type> & other) const;
+    inline vec3<Type> & operator-(const vec3<Type> & other) const;
+
+    inline vec3<Type> & dot  (const vec3<Type> & other) const;
+    inline vec3<Type> & cross(const vec3<Type> & other) const;
+
+    inline friend vec3<Type> & dot  (const vec3<Type> & a, const vec3<Type> & b);
+    inline friend vec3<Type> & cross(const vec3<Type> & a, const vec3<Type> & b);
   };
+
 };
